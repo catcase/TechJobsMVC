6d34cb0443447b406cd54c2c243af49508af3f08
Cat <ccase2008@yahoo.com>
2020-01-16 16:03:36 -0600

    It works?????

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -15,6 +15,24 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+        public IActionResult Results(string SearchTerm, string SearchType)
+        {
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.title = "Search";
 
+            if (SearchType.Equals("all"))
+            {
+                //Console.WriteLine("Search all fields not yet implemented.");
+                //pull in find by value data to search all jobs across columns
+                ViewBag.jobs = JobData.FindByValue(SearchTerm);
+            }
+            else
+            {
+                ViewBag.jobs = JobData.FindByColumnAndValue(SearchType, SearchTerm);
+            }
+
+            return View("Index");
+
+        }
     }
 }
